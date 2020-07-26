26582e39b756b6b85b869fd0f25dee9ee2b422fe
DeboDevelop <debajyotidofficial@gmail.com>
2020-07-26 19:13:48 +0530

    Added Change Colour Functionality and fix a bug with stroke weight

--- src/static/js/sketch.js
+++ src/static/js/sketch.js
@@ -1,6 +1,7 @@
 let cv;
 let socket;
 let sweight;
+let red,green,blue;
 
 //Function to setup everything
 function setup() {
@@ -8,6 +9,9 @@ function setup() {
   cv.parent('myContainer');
   background(51);
   sweight = 5;
+  red=255;
+  green=255;
+  blue=255;
 
   socket = io.connect('http://localhost:3000');
   socket.on('mouse', newDrawing);
@@ -19,8 +23,8 @@ function setup() {
 
 //Function to draw the receiving data
 function newDrawing(data) {
-  stroke(255, 0, 100);
-  strokeWeight(sweight)
+  stroke(`rgba(${red}, ${green}, ${blue}, 0.8)`);
+  strokeWeight(data.weight)
 	line(data.x, data.y, data.px, data.py)
 }
 
@@ -30,8 +34,39 @@ function clearDrawing() {
   socket.emit('clear');
 }
 
+//Function to change the stroke
 function changeWeight() {
   sweight  = document.getElementById("weight").value;
+}
+
+//Function to Change color
+function changeColour() {
+  let selectedColour = document.getElementById("lineColor").value;
+  if(selectedColour==='White') {
+    red=255;
+    green=255;
+    blue=255;
+  } else if (selectedColour==='Sky Blue')
+  {
+    red=0;
+    green=128;
+    blue=255;
+  } else if(selectedColour==='Pink')
+  {
+    red=255;
+    green=153;
+    blue=255;
+  } else if(selectedColour==='Yellow')
+  {
+    red=255;
+    green=255;
+    blue=0;
+  } else if(selectedColour==='Green')
+  {
+    red=0;
+    green=255;
+    blue=0;
+  }
 }
 
 //Function to draw
@@ -41,12 +76,13 @@ function mouseDragged() {
     x: mouseX,
     y: mouseY,
     px: pmouseX,
-    py: pmouseY
+    py: pmouseY,
+    weight: sweight
   };
 
   socket.emit('mouse', data);
 
-  stroke(255);
+  stroke(red,green,blue);
   strokeWeight(sweight)
 	line(mouseX, mouseY, pmouseX, pmouseY)
 }
